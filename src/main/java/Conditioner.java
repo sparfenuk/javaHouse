d0d822d04b65deaad2cb897e5a58997bd8f71704
kalchenia <vladuk383@gmail.com>
2019-04-10 20:28:43 +0300

    Classes

---
+++ src/main/java/Conditioner.java
@@ -0,0 +1,31 @@
+import java.util.Date;
+
+public class Conditioner extends Devices {
+    public String Inclusion;
+    public int Temperature;
+    public Date time;
+
+    public String getInclusion() {
+        return Inclusion;
+    }
+
+    public void setInclusion(String inclusion) {
+        Inclusion = inclusion;
+    }
+
+    public int getTemperature() {
+        return Temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        Temperature = temperature;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+}
